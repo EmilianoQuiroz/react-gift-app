0f4032528fd68f4cf75230a6bada81b9e4e4b6e7
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-22 21:11:53 -0300

    Estilos css del componente GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,12 @@
 export const GifExpertApp = () => {
   return (
     <>
+        {/* Titulo */}
         <h1>Gif Expert App</h1>
+        {/* Input */}
+
+        {/* Listado de Gif */}
+            {/* Gif Item */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GifExpertApp } from './GifExpertApp'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+{/* Importamos los estilos css */}
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
