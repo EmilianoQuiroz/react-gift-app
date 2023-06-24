87eea8b08ad1a39d7a2b48fc93b17b8f98eee166
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-23 23:22:47 -0300

    Creacion de lista de tareas

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,25 @@
+//Importamos el UseState
+import { useState } from "react";
 
 export const GifExpertApp = () => {
-  return (
+
+const [categories, setCategories] = useState(['One Punch', 'One Piece']);
+
+console.log(categories);
+
+return (
     <>
         {/* Titulo */}
         <h1>Gif Expert App</h1>
         {/* Input */}
 
         {/* Listado de Gif */}
+        <ol>
+            { categories.map( category => {
+                return <li key={category}> { category } </li>
+                }) 
+            }
+        </ol>
             {/* Gif Item */}
     </>
   )
