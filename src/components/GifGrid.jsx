4771de0aaca8fa9fcc17478026b56470e1694bb0
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-25 20:08:10 -0300

    Mostrar los titulos de la categoria

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
+    const getImages = async() => {
+        const newImages = await getGifs( category );
+        setImages( newImages );
+    }
     useEffect (() =>{
-        getGifs(category);
+        getImages();
     }, []);
 
    return (
     <>
         <h3>{ category }</h3>
-        <p>Hola mundo</p>
+        <ol>
+            {
+                images.map( ({ id, title}) =>(
+                    <li key={ id }>{ title }</li>
+                ))
+            }
+        </ol>
     </>
   )
 }
