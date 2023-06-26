f16fae83eaaf8a42293fc8b00ea67968ec8f6f36
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-06-25 22:29:51 -0300

    Implementacion de Custom Hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from 'react';
 import { GifItem } from './GifItem';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setImages] = useState([]);
-
-    const getImages = async() => {
-        const newImages = await getGifs( category );
-        setImages( newImages );
-    }
-    useEffect (() =>{
-        getImages();
-    }, []);
+    // Custom Hook
+    const { images, isLoading } = useFetchGifs( category );
 
    return (
     <>
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async ( category ) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=CQHPXRjqrmj2G3IvhaiyZmX1VyI2HC4T&q=${ category }&limit=30`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=CQHPXRjqrmj2G3IvhaiyZmX1VyI2HC4T&q=${ category }&limit=13`;
     const resp = await fetch( url );
     const { data } = await resp.json();
     const gifs = data.map(img => ({
